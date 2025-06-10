b9ee85ca23372f04670036e188e3f56a87611601
Swathi Amaravadi <swathiamaravadi53@gmail.com>
2025-06-10 12:08:11 -0400

    git add . git commit -m "Maven project setup" git push

--- src/main/java/com/springboot/example/springbootdeepdive/SpringBootDeepDiveApplication.java
+++ src/main/java/com/springboot/example/springbootdeepdive/SpringBootDeepDiveApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDeepDiveApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDeepDiveApplication.class, args);
-    }
+        System.out.println("Hello");
+          }
 
 }
