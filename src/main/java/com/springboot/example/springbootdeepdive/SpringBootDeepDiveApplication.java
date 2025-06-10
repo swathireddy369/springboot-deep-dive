454aa3b85f88305a60146e50ee7ebd83aa3e03d1
Swathi Amaravadi <swathiamaravadi53@gmail.com>
2025-06-10 12:00:21 -0400

    git add .

---
+++ src/main/java/com/springboot/example/springbootdeepdive/SpringBootDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.example.springbootdeepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeepDiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeepDiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/example/springbootdeepdive/SpringBootDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.example.springbootdeepdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDeepDiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
